d2c3023ffc08d456cf40d5bc4530503457c007dc
Aro Ayomide <skilashiaro@gmail.com>
2020-02-12 20:19:08 +0100

    Added stripe implementation

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def create_dag():
                     dag_file.write(content)
 
                 with open("facebook_backlog_dag_template.py", 'r') as backlog_template_content:
-                    #backlog_ddd = date.today() + timedelta(days=1)
+                    # backlog_ddd = date.today() + timedelta(days=1)
                     backlog_ddd = date.today()
                     backlog_content = backlog_template_content.read().replace("START_DATE",
                                                                               "datetime({year}, {month}, {day})".format(
@@ -98,8 +98,10 @@ def create_dag():
                 restart_command = """cd /home/ubuntu/docker-airflow &&  sudo docker restart docker-airflow_webserver_1 """
                 os.system(restart_command)
                 time.sleep(3)
-                unpause_dag = """cd /home/ubuntu/docker-airflow &&  sudo docker-compose run --rm webserver airflow unpause {DAG_ID}""".format(DAG_ID="act_{account_id}_daily_run_dag".format(account_id=account_id))
-                unpause_backlog_dag = """cd /home/ubuntu/docker-airflow &&  sudo docker-compose run --rm webserver airflow unpause {DAG_ID}""".format(DAG_ID="act_{account_id}_backlog_run_dag".format(account_id=account_id))
+                unpause_dag = """cd /home/ubuntu/docker-airflow &&  sudo docker-compose run --rm webserver airflow unpause {DAG_ID}""".format(
+                    DAG_ID="act_{account_id}_daily_run_dag".format(account_id=account_id))
+                unpause_backlog_dag = """cd /home/ubuntu/docker-airflow &&  sudo docker-compose run --rm webserver airflow unpause {DAG_ID}""".format(
+                    DAG_ID="act_{account_id}_backlog_run_dag".format(account_id=account_id))
                 os.system(unpause_dag)
                 time.sleep(1)
                 os.system(unpause_backlog_dag)
@@ -115,6 +117,129 @@ def create_dag():
             time.sleep(1)
             restart_command = """cd /home/ubuntu/docker-airflow &&  sudo docker restart docker-airflow_webserver_1 """
             os.system(restart_command)
+
+        return 'Request For Dag Creation has been sent'
+    else:
+        return 'No account id found'
+
+
+@app.route('/create_stripe_dag', methods=['GET'])
+def create_dag():
+    account_name = request.args.get('account_name')
+    details = request.args.get('details')
+    event_type = request.args.get('event_type')
+    print("Account Name ", account_name)
+    print("Details ID ", details)
+    if account_name:
+        if event_type == "creation":
+            if os.path.exists(
+                    '{BASE_PATH}stripe_{account_name}_charge_dag.py'.format(account_name=account_name, BASE_PATH=BASE_PATH)):
+                pass
+            else:
+                file_name = "stripe_{account_name}_variables".format(account_name=account_name)
+                variable_definition = {
+                    "stripe_{account_name}_dag_variables".format(account_name=account_name): json.loads(details)
+                }
+                with open("stripe_account_charge_dag.py", 'r') as charge_template_content:
+                    ddd = date.today()
+                    charge_content = charge_template_content.read().replace("START_DATE",
+                                                                            "datetime({year}, {month}, {day})".format(
+                                                                                year=ddd.year,
+                                                                                month=ddd.month,
+                                                                                day=ddd.day))
+                    charge_content = charge_content.replace('DAG_NAME',
+                                                            "stripe_{account_name}_charge_dag".format(
+                                                                account_name=account_name))
+                    charge_content = charge_content.replace('VARIABLES_NAME',
+                                                            "stripe_{account_name}_dag_variables".format(
+                                                                account_name=account_name))
+                    charge_content = charge_content.replace('SCHEDULE_INTERVAL', "0 */4 * * *")
+
+                with open("{BASE_PATH}stripe_{account_name}_charge_dag.py".format(BASE_PATH=BASE_PATH,
+                                                                                  account_name=account_name),
+                          'w') as charge_dag_file:
+                    charge_dag_file.write(charge_content)
+
+                with open("stripe_account_dispute_dag.py", 'r') as dispute_template_content:
+                    ddd = date.today()
+                    dispute_content = dispute_template_content.read().replace("START_DATE",
+                                                                            "datetime({year}, {month}, {day})".format(
+                                                                                year=ddd.year,
+                                                                                month=ddd.month,
+                                                                                day=ddd.day))
+                    dispute_content = dispute_content.replace('DAG_NAME',
+                                                            "stripe_{account_name}_dispute_dag".format(
+                                                                account_name=account_name))
+                    dispute_content = dispute_content.replace('VARIABLES_NAME',
+                                                            "stripe_{account_name}_dag_variables".format(
+                                                                account_name=account_name))
+                    dispute_content = dispute_content.replace('SCHEDULE_INTERVAL', "0 */4 * * *")
+
+                with open("{BASE_PATH}stripe_{account_name}_dispute_dag.py".format(BASE_PATH=BASE_PATH,
+                                                                                  account_name=account_name),
+                          'w') as dispute_dag_file:
+                    dispute_dag_file.write(dispute_content)
+
+                with open("stripe_account_refund_dag.py", 'r') as refund_template_content:
+                    ddd = date.today()
+                    refund_content = refund_template_content.read().replace("START_DATE",
+                                                                            "datetime({year}, {month}, {day})".format(
+                                                                                year=ddd.year,
+                                                                                month=ddd.month,
+                                                                                day=ddd.day))
+                    refund_content = refund_content.replace('DAG_NAME',
+                                                            "stripe_{account_name}_refund_dag".format(
+                                                                account_name=account_name))
+                    refund_content = refund_content.replace('VARIABLES_NAME',
+                                                            "stripe_{account_name}_dag_variables".format(
+                                                                account_name=account_name))
+                    refund_content = refund_content.replace('SCHEDULE_INTERVAL', "0 */4 * * *")
+
+                with open("{BASE_PATH}stripe_{account_name}_refund_dag.py".format(BASE_PATH=BASE_PATH,
+                                                                                  account_name=account_name),
+                          'w') as refund_dag_file:
+                    refund_dag_file.write(refund_content)
+
+
+                with open("{BASE_PATH}config/{filename}.json".format(BASE_PATH=BASE_PATH,
+                                                                     filename=file_name),
+                          'w') as variable_file:
+                    variable_file.write(json.dumps(variable_definition, indent=4))
+
+                time.sleep(2)
+
+                set_variable_command = """cd /home/ubuntu/docker-airflow &&  sudo docker-compose run --rm webserver airflow variables --import /usr/local/airflow/dags/config/{file_name}.json """.format(
+                    file_name=file_name)
+                os.system(set_variable_command)
+
+                time.sleep(1)
+                restart_command = """cd /home/ubuntu/docker-airflow &&  sudo docker restart docker-airflow_webserver_1 """
+                os.system(restart_command)
+                time.sleep(3)
+                unpause_charge_dag = """cd /home/ubuntu/docker-airflow &&  sudo docker-compose run --rm webserver airflow unpause {DAG_ID}""".format(
+                    DAG_ID="stripe_{account_name}_charge_dag".format(account_name=account_name))
+                unpause_dispute_dag = """cd /home/ubuntu/docker-airflow &&  sudo docker-compose run --rm webserver airflow unpause {DAG_ID}""".format(
+                    DAG_ID="stripe_{account_name}_dispute_dag".format(account_name=account_name))
+                unpause_refund_dag = """cd /home/ubuntu/docker-airflow &&  sudo docker-compose run --rm webserver airflow unpause {DAG_ID}""".format(
+                    DAG_ID="stripe_{account_name}_refund_dag".format(account_name=account_name))
+                os.system(unpause_charge_dag)
+                time.sleep(1)
+                os.system(unpause_dispute_dag)
+                time.sleep(1)
+                os.system(unpause_refund_dag)
+
+        elif event_type == "deletion":
+            pass
+            # deletion_command = """cd /home/ubuntu/docker-airflow && sudo rm -rf /home/ubuntu/docker-airflow/dags/act_{account_id}*""".format(
+            #     account_id=account_id)
+            # os.system(deletion_command)
+            # time.sleep(1)
+            # deletion_var_command = """cd /home/ubuntu/docker-airflow && sudo rm -f /home/ubuntu/docker-airflow/dags/config/act_{account_id}_variables.json""".format(
+            #     account_id=account_id)
+            # os.system(deletion_var_command)
+            # time.sleep(1)
+            # restart_command = """cd /home/ubuntu/docker-airflow &&  sudo docker restart docker-airflow_webserver_1 """
+            # os.system(restart_command)
 
         return 'Request For Dag Creation has been sent'
     else:
---
+++ stripe_account_dispute_dag.py
@@ -0,0 +1,162 @@
+__author__ = 'Ayomide Aro'
+
+import json
+import stripe
+import redis
+import os
+
+from airflow import DAG
+from airflow.models import Variable
+
+from datetime import timedelta
+from datetime import datetime, date
+import time
+
+from airflow.operators.bash_operator import BashOperator
+from airflow.hooks.base_hook import BaseHook
+
+from airflow.operators.python_operator import PythonOperator
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': START_DATE,
+    'email': ['admin@harwardmedia.com'],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=3),
+}
+
+dag_name = "DAG_NAME"
+dag = DAG(dag_name, catchup=False, default_args=default_args, schedule_interval="SCHEDULE_INTERVAL")
+# dag = DAG('act_1259601010838576_daily_run_dag', default_args=default_args, schedule_interval=None)
+
+dag_config = Variable.get('VARIABLES_NAME', deserialize_json=True)
+stripe.api_key = dag_config['stripe_key']
+aws_conn = BaseHook.get_connection("aws_conn")
+s3_bucket = dag_config['s3_bucket']
+datasource_type = dag_config['datasource_type']
+account_name = dag_config['account_name']
+file_path = dag_config['file_path']
+s3_folder_path = "/{datasource_type}/{transaction_type}/{account_name}/{year}/{month}/{day}/"
+
+# file_key = 'act_{0}_DAY.json'.format(account_name)
+file_key_regex = 'stripe_dispute_{0}/'.format(account_name)
+# s3_key = 'act_{}'.format(account_name)
+
+print("Starting job rn ")
+
+timestamp = int(time.time() * 1000.0)
+
+date_key = date.today()
+
+date_key_arr = str(date_key).split("-")
+year = date_key_arr[0]
+month = date_key_arr[1]
+dayy = date_key_arr[2]
+
+s3_folder = s3_folder_path.format(datasource_type=datasource_type, transaction_type='dispute',
+                                  account_name=account_name,
+                                  year=year,
+                                  month=month, day=dayy)
+
+
+def get_insight_from_stripe(ds, **kwargs):
+    r = redis.Redis(host='redis', port=6379, db=0)
+
+    if r.exists(dag_name) == 1:
+        filename = 'dispute_data_{0}.json'.format(timestamp)
+        f = open(file_path + file_key_regex + filename, 'w+')
+        new_data = stripe.Dispute.list(ending_before=r.hget(dag_name, "last_obj_id"))
+        if len(new_data['data']) == 0:
+            f.close()
+            return filename
+        else:
+            last_obj_id = new_data['data'][0]['id']
+
+            print(last_obj_id)
+
+            for new_data_item in new_data.auto_paging_iter():
+                f.write(json.dumps(new_data_item) + "\n")
+
+            f.close()
+
+            r.hset(dag_name, "last_obj_id", last_obj_id)
+            return filename
+    else:
+        filename = 'dispute_data_{0}.json'.format(timestamp)
+        f = open(file_path + file_key_regex + filename, 'w+')
+        backlog_data = stripe.Dispute.list()
+        if len(backlog_data['data']) == 0:
+            f.close()
+            return filename
+        else:
+            last_obj_id = backlog_data['data'][0]['id']
+
+            for back_log_d in backlog_data.auto_paging_iter():
+                f.write(json.dumps(back_log_d) + "\n")
+
+            f.close()
+            r.hset(dag_name, "last_obj_id", last_obj_id)
+            return filename
+
+
+def upload_dispute_data_to_s3_bucket(**kwargs):
+    date_key = date.today()
+
+    date_key_arr = str(date_key).split("-")
+    year = date_key_arr[0]
+    month = date_key_arr[1]
+    dayy = date_key_arr[2]
+    s3_folder = s3_folder_path.format(datasource_type=datasource_type, transaction_type='dispute',
+                                      account_name=account_name,
+                                      year=year,
+                                      month=month, day=dayy)
+
+    fff = kwargs['ti'].xcom_pull(task_ids='get_insight_from_stripe')
+    file_to_upload = "{filepath}".format(filepath=file_path + file_key_regex) + fff
+
+    s3_path_to_upload = """s3://{s3_bucket}{s3_key}{fff}""".format(
+        s3_bucket=s3_bucket, s3_key=s3_folder, fff=fff)
+
+    if os.stat(file_to_upload).st_size == 0:
+        return True
+    else:
+        upload_command = """AWS_ACCESS_KEY_ID={access_key} AWS_SECRET_ACCESS_KEY={secret_key} aws s3 mv {file_to_upload} {s3_path_to_upload}""".format(
+            s3_path_to_upload=s3_path_to_upload, file_to_upload=file_to_upload,
+            access_key=aws_conn.extra_dejson['aws_access_key_id'],
+            secret_key=aws_conn.extra_dejson['aws_secret_access_key'])
+        os.system(upload_command)
+        time.sleep(1)
+        return True
+
+
+t3 = PythonOperator(
+    task_id='upload_dispute_data_to_s3_bucket',
+    python_callable=upload_dispute_data_to_s3_bucket,
+    provide_context=True,
+    dag=dag
+)
+
+t2 = PythonOperator(
+    task_id='get_insight_from_stripe',
+    python_callable=get_insight_from_stripe,
+    provide_context=True,
+    dag=dag)
+
+
+def confirm_parameters(**kwargs):
+    if not os.path.exists(file_path + file_key_regex):
+        os.makedirs(file_path + file_key_regex)
+    print(dag_config)
+    return True
+
+
+t1 = PythonOperator(
+    task_id='confirm_params',
+    python_callable=confirm_parameters,
+    dag=dag
+)
+
+t1 >> t2 >> t3
