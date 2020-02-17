984218c6f184eb5058ddee84823ecce8244aa74f
Aro Ayomide <skilashiaro@gmail.com>
2020-02-17 01:18:03 +0100

    Added klarna integration

--- app.py
+++ app.py
@@ -229,17 +229,16 @@ def create_stripe_dag():
                 os.system(unpause_refund_dag)
 
         elif event_type == "deletion":
-            pass
-            # deletion_command = """cd /home/ubuntu/docker-airflow && sudo rm -rf /home/ubuntu/docker-airflow/dags/act_{account_id}*""".format(
-            #     account_id=account_id)
-            # os.system(deletion_command)
-            # time.sleep(1)
-            # deletion_var_command = """cd /home/ubuntu/docker-airflow && sudo rm -f /home/ubuntu/docker-airflow/dags/config/act_{account_id}_variables.json""".format(
-            #     account_id=account_id)
-            # os.system(deletion_var_command)
-            # time.sleep(1)
-            # restart_command = """cd /home/ubuntu/docker-airflow &&  sudo docker restart docker-airflow_webserver_1 """
-            # os.system(restart_command)
+            deletion_command = """cd /home/ubuntu/docker-airflow && sudo rm -rf /home/ubuntu/docker-airflow/dags/stripe_{account_name}*""".format(
+                account_name=account_name)
+            os.system(deletion_command)
+            time.sleep(1)
+            deletion_var_command = """cd /home/ubuntu/docker-airflow && sudo rm -f /home/ubuntu/docker-airflow/dags/config/stripe_{account_name}_variables.json""".format(
+                account_name=account_name)
+            os.system(deletion_var_command)
+            time.sleep(1)
+            restart_command = """cd /home/ubuntu/docker-airflow &&  sudo docker restart docker-airflow_webserver_1 """
+            os.system(restart_command)
 
         return 'Request For Dag Creation has been sent'
     else:
@@ -304,7 +303,90 @@ def create_shopify_dag():
                 os.system(unpause_charge_dag)
 
         elif event_type == "deletion":
-            pass
+            deletion_command = """cd /home/ubuntu/docker-airflow && sudo rm -rf /home/ubuntu/docker-airflow/dags/shopify_{shop_name}*""".format(
+                shop_name=shop_name)
+            os.system(deletion_command)
+            time.sleep(1)
+            deletion_var_command = """cd /home/ubuntu/docker-airflow && sudo rm -f /home/ubuntu/docker-airflow/dags/config/shopify_{shop_name}_variables.json""".format(
+                shop_name=shop_name)
+            os.system(deletion_var_command)
+            time.sleep(1)
+            restart_command = """cd /home/ubuntu/docker-airflow &&  sudo docker restart docker-airflow_webserver_1 """
+            os.system(restart_command)
+
+        return 'Request For Dag Creation has been sent'
+    else:
+        return 'No account id found'
+
+
+@app.route('/create_klarna_dag', methods=['GET'])
+def create_klarna_dag():
+    account_name = request.args.get('account_name')
+    details = request.args.get('details')
+    event_type = request.args.get('event_type')
+    print("Account Name ", account_name)
+    print("Details ID ", details)
+    if account_name:
+        if event_type == "creation":
+            if os.path.exists(
+                    '{BASE_PATH}klarna_{account_name}_transaction_dag.py'.format(account_name=account_name,
+                                                                                 BASE_PATH=BASE_PATH)):
+                pass
+            else:
+                file_name = "klarna_{account_name}_variables".format(account_name=account_name)
+                variable_definition = {
+                    "klarna_{account_name}_dag_variables".format(account_name=account_name): json.loads(details)
+                }
+                with open("klarna_account_dag.py", 'r') as transaction_template_content:
+                    ddd = date.today()
+                    transaction_content = transaction_template_content.read().replace("START_DATE",
+                                                                                      "datetime({year}, {month}, {day})".format(
+                                                                                          year=ddd.year,
+                                                                                          month=ddd.month,
+                                                                                          day=ddd.day))
+                    transaction_content = transaction_content.replace('DAG_NAME',
+                                                                      "klarna_{account_name}_transaction_dag".format(
+                                                                          account_name=account_name))
+                    transaction_content = transaction_content.replace('VARIABLES_NAME',
+                                                                      "klarna_{account_name}_dag_variables".format(
+                                                                          account_name=account_name))
+                    transaction_content = transaction_content.replace('SCHEDULE_INTERVAL', "*/15 * * * *")
+
+                with open("{BASE_PATH}klarna_{account_name}_transaction_dag.py".format(BASE_PATH=BASE_PATH,
+                                                                                       account_name=account_name),
+                          'w') as transaction_dag_file:
+                    transaction_dag_file.write(transaction_content)
+
+                with open("{BASE_PATH}config/{filename}.json".format(BASE_PATH=BASE_PATH,
+                                                                     filename=file_name),
+                          'w') as variable_file:
+                    variable_file.write(json.dumps(variable_definition, indent=4))
+
+                time.sleep(2)
+
+                set_variable_command = """cd /home/ubuntu/docker-airflow &&  sudo docker-compose run --rm webserver airflow variables --import /usr/local/airflow/dags/config/{file_name}.json """.format(
+                    file_name=file_name)
+                os.system(set_variable_command)
+
+                time.sleep(1)
+                restart_command = """cd /home/ubuntu/docker-airflow &&  sudo docker restart docker-airflow_webserver_1 """
+                os.system(restart_command)
+                time.sleep(3)
+                unpause_charge_dag = """cd /home/ubuntu/docker-airflow &&  sudo docker-compose run --rm webserver airflow unpause {DAG_ID}""".format(
+                    DAG_ID="klarna_{account_name}_transaction_dag".format(account_name=account_name))
+                os.system(unpause_charge_dag)
+
+        elif event_type == "deletion":
+            deletion_command = """cd /home/ubuntu/docker-airflow && sudo rm -rf /home/ubuntu/docker-airflow/dags/klarna_{account_name}*""".format(
+                account_name=account_name)
+            os.system(deletion_command)
+            time.sleep(1)
+            deletion_var_command = """cd /home/ubuntu/docker-airflow && sudo rm -f /home/ubuntu/docker-airflow/dags/config/klarna_{account_name}_variables.json""".format(
+                account_name=account_name)
+            os.system(deletion_var_command)
+            time.sleep(1)
+            restart_command = """cd /home/ubuntu/docker-airflow &&  sudo docker restart docker-airflow_webserver_1 """
+            os.system(restart_command)
 
         return 'Request For Dag Creation has been sent'
     else:
---
+++ klarna_account_dag.py
@@ -0,0 +1,223 @@
+__author__ = 'Ayomide Aro'
+
+import json
+import stripe
+import redis
+import os
+import requests
+
+from airflow import DAG
+from airflow.models import Variable
+
+from datetime import timedelta
+from datetime import datetime, date
+import time
+
+from airflow.operators.bash_operator import BashOperator
+from airflow.hooks.base_hook import BaseHook
+
+from airflow.operators.python_operator import PythonOperator
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': START_DATE,
+    'email': ['admin@harwardmedia.com'],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 3,
+    'wait_for_downstream': True,
+    'retry_delay': timedelta(minutes=3),
+}
+
+dag_name = "DAG_NAME"
+dag = DAG(dag_name, catchup=False, default_args=default_args, schedule_interval="SCHEDULE_INTERVAL")
+
+dag_config = Variable.get('VARIABLES_NAME', deserialize_json=True)
+klarna_username = dag_config['klarna_username']
+klarna_password = dag_config['klarna_password']
+aws_conn = BaseHook.get_connection("aws_conn")
+s3_bucket = dag_config['s3_bucket']
+datasource_type = dag_config['datasource_type']
+account_name = dag_config['account_name']
+file_path = dag_config['file_path']
+s3_folder_path = "/{datasource_type}/{transaction_type}/{account_name}/{year}/{month}/{day}/"
+
+file_key_regex = 'klarna_transaction_{0}/'.format(account_name)
+
+print("Starting job rn ")
+
+timestamp = int(time.time() * 1000.0)
+
+BASE_URL = "https://api-na.klarna.com"
+
+transaction_url = BASE_URL + "/settlements/v1/transactions"
+
+
+def confirm_parameters(**kwargs):
+    if not os.path.exists(file_path + file_key_regex):
+        os.makedirs(file_path + file_key_regex)
+    print(dag_config)
+    return True
+
+
+def get_insight_from_klarna(ds, **kwargs):
+    r = redis.Redis(host='redis', port=6379, db=1)
+
+    session = requests.Session()
+    session.auth = (klarna_username, klarna_password)
+
+    if r.exists(dag_name) == 1:
+        transactions = []
+        response = session.get(transaction_url + "?offset={offset}".format(offset=int(r.hget(dag_name, "offset"))))
+        insight_response_body = response.json()
+
+        print("Subsequent transaction body ", insight_response_body)
+
+        pagination = insight_response_body['pagination']
+        total = pagination['total']
+
+        if len(insight_response_body['transactions']) > 0:
+            count = 0
+
+            while 'next' in insight_response_body.get('pagination', {}):
+                time.sleep(2)
+                transactions.extend(insight_response_body['transactions'])
+                response = session.get(insight_response_body['pagination']['next'])
+                print("Next url count {count} ".format(count=count), insight_response_body['pagination'])
+                response.raise_for_status()
+                insight_response_body = response.json()
+                count = count + 1
+
+            transactions.extend(insight_response_body['transactions'])
+
+            r.hset(dag_name, "offset", total)
+
+            session.close()
+
+            return transactions
+        else:
+            return transactions
+    else:
+        transactions = []
+        response = session.get(transaction_url)
+        insight_response_body = response.json()
+
+        print("Initial transaction body ", insight_response_body)
+
+        pagination = insight_response_body['pagination']
+        total = pagination['total']
+
+        if total > 0:
+            count = 0
+
+            while 'next' in insight_response_body.get('pagination', {}):
+                time.sleep(2)
+                transactions.extend(insight_response_body['transactions'])
+                response = session.get(insight_response_body['pagination']['next'])
+                print("Next url count {count} ".format(count=count), insight_response_body['pagination'])
+                response.raise_for_status()
+                insight_response_body = response.json()
+                count = count + 1
+
+            transactions.extend(insight_response_body['transactions'])
+
+            r.hset(dag_name, "offset", total)
+
+            return transactions
+        else:
+            return transactions
+
+
+def write_transaction_to_file(**kwargs):
+    charge_filename = 'charge_data_{0}.json'.format(timestamp)
+    refund_filename = 'refund_data_{0}.json'.format(timestamp)
+    dispute_filename = 'dispute_data_{0}.json'.format(timestamp)
+    fee_filename = 'fee_data_{0}.json'.format(timestamp)
+    charge_f = open(file_path + file_key_regex + charge_filename, 'w+')
+    refund_f = open(file_path + file_key_regex + refund_filename, 'w+')
+    dispute_f = open(file_path + file_key_regex + dispute_filename, 'w+')
+    fee_f = open(file_path + file_key_regex + fee_filename, 'w+')
+
+    trxs = kwargs['ti'].xcom_pull(task_ids='get_insight_from_klarna')
+    for trx in trxs:
+        if trx['type'] == "SALE":
+            charge_f.write(json.dumps(trx) + "\n")
+        elif trx['type'] == "RETURN":
+            refund_f.write(json.dumps(trx) + "\n")
+        elif trx['type'] == "REVERSAL":
+            dispute_f.write(json.dumps(trx) + "\n")
+        elif trx['type'] == "FEE":
+            fee_f.write(json.dumps(trx) + "\n")
+
+    charge_f.close()
+    refund_f.close()
+    dispute_f.close()
+    fee_f.close()
+
+    return_data = [
+        {"type": "charge", "path": file_path + file_key_regex + charge_filename, "filename": charge_filename},
+        {"type": "refund", "path": file_path + file_key_regex + refund_filename, "filename": refund_filename},
+        {"type": "dispute", "path": file_path + file_key_regex + dispute_filename, "filename": dispute_filename},
+        {"type": "fee", "path": file_path + file_key_regex + fee_filename, "filename": fee_filename},
+    ]
+
+    return return_data
+
+
+def upload_transaction_data_to_s3_bucket(**kwargs):
+    date_key = date.today()
+
+    date_key_arr = str(date_key).split("-")
+    year = date_key_arr[0]
+    month = date_key_arr[1]
+    dayy = date_key_arr[2]
+
+    files_to_upload = kwargs['ti'].xcom_pull(task_ids='write_transaction_to_file')
+    for file_to_upload in files_to_upload:
+        if os.stat(file_to_upload['path']).st_size == 0:
+            pass
+        else:
+            s3_folder = s3_folder_path.format(datasource_type=datasource_type, transaction_type=file_to_upload['type'],
+                                              account_name=account_name,
+                                              year=year,
+                                              month=month, day=dayy)
+            s3_path_to_upload = """s3://{s3_bucket}{s3_key}{fff}""".format(
+                s3_bucket=s3_bucket, s3_key=s3_folder, fff=file_to_upload['filename'])
+
+            upload_command = """AWS_ACCESS_KEY_ID={access_key} AWS_SECRET_ACCESS_KEY={secret_key} aws s3 mv {file_to_upload} {s3_path_to_upload}""".format(
+                s3_path_to_upload=s3_path_to_upload, file_to_upload=file_to_upload['path'],
+                access_key=aws_conn.extra_dejson['aws_access_key_id'],
+                secret_key=aws_conn.extra_dejson['aws_secret_access_key'])
+            os.system(upload_command)
+            time.sleep(1)
+    return True
+
+
+t4 = PythonOperator(
+    task_id='upload_transaction_data_to_s3_bucket',
+    python_callable=upload_transaction_data_to_s3_bucket,
+    provide_context=True,
+    dag=dag
+)
+
+t3 = PythonOperator(
+    task_id='write_transaction_to_file',
+    python_callable=write_transaction_to_file,
+    provide_context=True,
+    dag=dag
+)
+
+t2 = PythonOperator(
+    task_id='get_insight_from_klarna',
+    python_callable=get_insight_from_klarna,
+    provide_context=True,
+    dag=dag)
+
+t1 = PythonOperator(
+    task_id='confirm_params',
+    python_callable=confirm_parameters,
+    dag=dag
+)
+
+t1 >> t2 >> t3 >> t4
